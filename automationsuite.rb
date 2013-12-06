9e332845b41daff0f0d724997e422fd706bf9fb6
Faraaz Khan <faraaz@rationalizeit.us>
2013-12-05 23:42:08 -0500

    adding some exception handling

--- automationsuite.rb
+++ automationsuite.rb
@@ -15,8 +15,8 @@
 # Install Gherkin Editor
 require 'net/http'
 class Installer
-
   def self.install(old_installation=true)
+    @attempts = @attempts.to_i
     if ruby_installed?
       puts ruby_installed_message if old_installation
       if framework_installed?
@@ -27,16 +27,26 @@ class Installer
         framework_post_install
       end
     else
+      raise "There was a problem with the installation. Please file a bug report at bugreports.cgifederal.com" if @attempts > 0
       puts "Installing Ruby...this will take upto 15 minutes...be patient"
+      @attempts += 1
       install_ruby
+      sleep 5
+      puts "Ruby is installed" if ruby_installed?
       self.install(false)
     end
     #download_and_install(editor)
   end
 
   def self.ruby_installed?
-    @ruby_path = run("where ruby")
-    !@ruby_path.to_s.empty?
+    @rubyversion = run("ruby -v")
+    if @rubyversion.match('1.9.3') 
+      true
+    elsif @rubyversion.match('ruby')
+      raise "You seem to have #{@rubyversion} installed \n. Please uninstall it and restart this installer"
+    else
+      false
+    end
   end
 
   def self.framework_installed?
@@ -56,7 +66,7 @@ class Installer
   def self.install_ruby
     #run download(binary), true
     file = File.join(File.dirname(__FILE__), 'railsinstaller.exe')
-    run file
+    run file rescue nil
   end
 
   def self.install_framework
@@ -70,13 +80,19 @@ class Installer
   end
 
   def self.ruby_installed_message
-    <<-message
-     You already have ruby installed at #{@ruby_path}. Please make sure it is version 1.9.3 by running `ruby -v`
+    correct_version = <<-message
+     It seems you already have ruby #{@rubyversion} installed. 
      Skipping installation of Ruby
      If you would like to reinstall ruby with this installer, uninstall existing ruby installation
      and run this installer again
     message
+
+    incorrect_version = "It seems that you have a different version of ruby installed. Please uninstall the current version \n
+    and rerun the installer"
+
+    @rubyversion.match('1.9.3') ? correct_version : incorrect_version
   end
+
 
   def self.framework_installed_message
     <<-message
